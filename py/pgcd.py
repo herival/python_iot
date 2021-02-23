4e57ff712ab231e09c598b5b631f948292687f5a
Heri <herimalala.val@gmail.com>
2021-02-24 00:21:58 +0100

    pgcd

--- py/pgcd.py
+++ py/pgcd.py
@@ -1,9 +1,16 @@
 # exercice pgcd trouver l'agorithme
 # recursive
 # PGCD(b,a%b)
-
 # exercice Trinome ax2+ bc + c
 
 # fonction convertisseur devises
 
+def pgcd(a, b):
+    if b > 0:
+        reste = a % b
+        return pgcd(b, reste)
+    else:
+        return a
 
+
+print(pgcd(64, 38))
