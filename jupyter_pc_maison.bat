4e5f794aba0bf55f87c5132f39888041a26a2ed7
Heri <herimalala.val@gmail.com>
2021-02-23 22:55:13 +0100

    first

---
+++ jupyter_pc_maison.bat
@@ -0,0 +1,8 @@
+cd D:\Dev\Greta92_python\python_iot\
+call D:\Dev\Greta92_python\environments\jupenv\Scripts\activate.bat
+jupyter notebook
+
+@REM cd %HOMEPATH%\environments\
+@REM call %HOMEPAHT%\environments\jupenv\Scripts\activate.bat
+
+pause
