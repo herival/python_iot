a14a55d6b9f9cbbeadfa225af79c0dd5de1871f7
Heri <herimalala.val@gmail.com>
2021-03-03 16:37:28 +0100

    03/03

--- corrections/fibonacci/fibonacci_it.py
+++ corrections/fibonacci/fibonacci_it.py
@@ -1,20 +1,25 @@
 # iterative
 
 def fib_i(N):
+    assert N>=0, "N doit être positif" # condition pour lever une exeption: si N est pas positif il renvoi l'exeption en message
+    global compteur
     x = 1
     y = 1
     z = 0
     if N == 0:
+        compteur +=1
         return 0
     elif N <= 2:
+        compteur +=1
         return 1
     for i in range(N-2):
+        compteur +=1
         z = x+y
         (x, y) = (y, z)
 
     return z
 
-
-# print(fib_i(10))
-for i in range(11):
-    print(fib_i(i))
+compteur = 0
+# print(fib_i(10), compteur)
+for i in range(10):
+    print(fib_i(i), compteur)
--- corrections/fibonacci/fibonacci_r.py
+++ corrections/fibonacci/fibonacci_r.py
@@ -1,10 +1,13 @@
 # recurcif
-
 def fib_r(n):
+    global compteur
+    compteur += 1
     if n < 2:
         return n
     return(fib_r(n-1)+fib_r(n-2))
 
+compteur = 0
+print(fib_r(10), compteur)
 
-for i in range(10):
-    print(fib_r(i))
+# for i in range(10):
+#     print(fib_r(i))
