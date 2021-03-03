cfb2d012d127409b86d89ed35a81c9cca468853f
Heri <herimalala.val@gmail.com>
2021-03-03 16:38:18 +0100

    03/03

--- corrections/decorateur/decorateur.py
+++ corrections/decorateur/decorateur.py
@@ -1,6 +1,34 @@
-var = 0
-while var < 6:
-    var += 1
-    if var % 2 == 0:
-        continue
-    print("#")
+def show_call(fonction):
+    # pour du multiple argument fonction_modifiée(*args, *kwargs):
+    # reste plus qu'à boucler sur les arges et les kwargs
+    # for a in args:
+    #     if s:
+    #         s+=","
+    #     s+= a
+    def fonction_modifiee(a, b):
+        print("convertir", a, b)
+        return fonction(a, b)
+    return fonction_modifiee
+
+
+@show_call
+def convertir(montant, taux):
+    print(taux*montant)
+
+# devise1 = EUR
+# devise2 = USD
+# tauxdechange = 0.9
+# montantaconvertir = 100
+# devise1 = input("Devise d'origine : ")
+# devise2 = input("Devise d'arrivée : ")
+tauxdechange = float(input("Taux de change : "))
+montantaconvertir = float(input("Montant à convertir : "))
+
+# print(montantaconvertir,
+#       devise1,
+#       "=",
+#       convertir(montantaconvertir,tauxdechange),
+#       devise2)
+
+convertir(tauxdechange, montantaconvertir)
+
